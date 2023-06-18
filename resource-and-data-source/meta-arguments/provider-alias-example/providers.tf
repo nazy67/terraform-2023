4a62c69a005934ea77678e28538e7172c7be85d5
nazy67 <nazykh67@gmail.com>
2023-06-17 21:57:47 -0500

    modified provider alias example

--- resource-and-data-source/meta-arguments/provider-alias-example/providers.tf
+++ resource-and-data-source/meta-arguments/provider-alias-example/providers.tf
@@ -8,6 +8,14 @@ terraform {
 }
 
 provider "aws" {
-  alias      = "amazon"
   region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+provider "aws" {
+  alias      = "west"
+  region     = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- resource-and-data-source/meta-arguments/provider-alias-example/resource.tf
+++ resource-and-data-source/meta-arguments/provider-alias-example/resource.tf
@@ -1,8 +1,16 @@
-resource "aws_instance" "ec2" {
-  provider      = aws.amazon
+resource "aws_instance" "server-us-east-1" {
   ami           = "ami-0715c1897453cabd1"
   instance_type = "t2.micro"
   tags = {
-    Name = "terraform-instance"
+    Name = "tf-instance-1"
+  }
+}
+
+resource "aws_instance" "server-us-west-2" {
+  provider      = aws.west
+  ami           = "ami-07dfed28fcf95241c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "tf-instance-2"
   }
 }
