b729358b56e8a64d6ace9f065f3b6b1cc473fb57
nazy67 <nazykh67@gmail.com>
2023-06-29 23:08:00 -0500

    added vpc config

---
+++ module/child-modules/vpc/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  common_tags = {
+    ManagedBy = "Terraform"
+    Owner     = "nazy.khalilova@gmail.com"
+    GitUrl    = "https://github.com/nazy67/terraform-2023/tree/main/homework"
+  }
+}
