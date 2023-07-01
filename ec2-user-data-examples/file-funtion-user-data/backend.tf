2fbfc9ce4b074c044b0022478520c6d97fc53427
nazy67 <nazykh67@gmail.com>
2023-06-30 23:37:35 -0500

    modified s3 backend

--- ec2-user-data-examples/file-funtion-user-data/backend.tf
+++ ec2-user-data-examples/file-funtion-user-data/backend.tf
@@ -1,26 +1,8 @@
-### Local backend ###
-# terraform {
-#   backend "local" {
-#     path = "state-folder/terraform.tfstate"
-#   }
-# }
-
 ### Remote backend ###
 terraform {
   backend "s3" {
-    bucket = "akumo-terraform-state-2023"
+    bucket = "akumo-terraform-state-2023-june"
     key    = "terraform/file-funtion-user-data.tfstate"
     region = "us-east-1"
   } 
 }
-
-### Terraform Cloud backend ###
-# terraform {
-#   backend "remote" {
-#     hostname = "app.terraform.io"
-#     organization = "nazy-main-org"
-#     workspaces {
-#       name = "non-production"
-#     }
-#   }
-# }
--- ec2-user-data-examples/inline-user-data/backend.tf
+++ ec2-user-data-examples/inline-user-data/backend.tf
@@ -1,7 +1,7 @@
 ### Remote backend ###
 terraform {
   backend "s3" {
-    bucket = "akumo-terraform-state-2023"
+    bucket = "akumo-terraform-state-2023-june"
     key    = "terraform/inline-user-data.tfstate"
     region = "us-east-1"
   }
